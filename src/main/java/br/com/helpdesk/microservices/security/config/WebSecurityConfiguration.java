87f091b0e01de198b6ab1839e1e68e97f960c3af
Joel Marcos <joel.cardoso@fatec.sp.gov.br>
2020-03-04 13:43:55 -0300

    Adicionando segurança no método createUser

--- src/main/java/br/com/helpdesk/microservices/security/config/WebSecurityConfiguration.java
+++ src/main/java/br/com/helpdesk/microservices/security/config/WebSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
--- src/main/java/br/com/helpdesk/microservices/security/controller/UserController.java
+++ src/main/java/br/com/helpdesk/microservices/security/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,7 @@ public class UserController {
 	private UserService userService;
 	
 	@PostMapping
+	@PreAuthorize("hasAnyRole('role_admin')")
 	public ResponseEntity<Void> createUser (@Valid @RequestBody NewUserDTO newUserDTO) {
 		User user = userService.fromDTO(newUserDTO);
 		userService.createUser(user);
